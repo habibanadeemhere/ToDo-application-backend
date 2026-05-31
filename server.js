9eeaeb8b7b12cee716fa8c954bdf4fbc44e75fdf
habibanadeemhere <habibanadeem075@gmail.com>
2026-05-30 23:04:50 -0700

    fixed cors

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 import app from "./api/index.js";
 import cors from "cors";
 
-app.use(
-  cors({
-    origin: "https://to-do-application-frontend-phi.vercel.app",
-    credentials: true,
-  })
-);
-
-app.options("*", cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://to-do-application-frontend-phi.vercel.app"
+  ],
+  credentials: true
+}));
 
 const PORT = process.env.PORT || 5000;
 
