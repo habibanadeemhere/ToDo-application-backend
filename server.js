90a036e268d8d0d06734a202198ad147ffe76f3d
habibanadeemhere <habibanadeem075@gmail.com>
2026-05-30 23:03:22 -0700

    fixed cors

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 import app from "./api/index.js";
+import cors from "cors";
+
+app.use(
+  cors({
+    origin: "https://to-do-application-frontend-phi.vercel.app",
+    credentials: true,
+  })
+);
+
+app.options("*", cors());
 
 const PORT = process.env.PORT || 5000;
 
